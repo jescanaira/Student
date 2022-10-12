7df32141ddd3d68c1849acbba9545281261d8204
jescanaira <115530262+jescanaira@users.noreply.github.com>
2022-10-12 15:47:18 -0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("marks[3]");
         int[]marks=new int[10];
-        marks[0]=20;
+        marks[0]=11;
         marks[1]=12;
         marks[2]=34;
-        marks[3]=16;
+        marks[3]=30;
         marks[4]=40;
         marks[5]=18;
         marks[6]=21;
-        marks[7]=22;
+        marks[7]=23;
         marks[8]=24;
         marks[9]=30;
-        System.out.println(marks[3]);
+        System.out.println(marks[5]);
     }
 }
