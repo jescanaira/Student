4e1ba95c64e8e706216c42e04fb0244c86795487
jescanaira <115530262+jescanaira@users.noreply.github.com>
2022-10-12 14:40:32 -0700

    do it

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("marks[3]");
+        int[]marks=new int[10];
+        marks[0]=20;
+        marks[1]=12;
+        marks[2]=34;
+        marks[3]=16;
+        marks[4]=40;
+        marks[5]=18;
+        marks[6]=21;
+        marks[7]=22;
+        marks[8]=24;
+        marks[9]=30;
+        System.out.println(marks[3]);
+    }
+}
